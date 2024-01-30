3770b6b53d6d0f9426d064802547088b4ae0c697
vaibhav-soni-learnapp <125342282+vaibhav-soni-learnapp@users.noreply.github.com>
2024-01-30 19:31:46 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -22,12 +22,12 @@ def calculate_financials(start_year, monthly_income, monthly_expenses, salary_in
 # Streamlit UI
 st.title('Financial Projection Calculator')
 
-start_year = st.number_input('Started Working Year', min_value=2000, max_value=2024, value=2018)
-monthly_income = st.number_input('Monthly Income', value=21000)
-monthly_expenses = st.number_input('Monthly Expenses', value=18000)
-salary_increment = st.slider('Salary Increment (%)', 0, 100, 10) / 100
-expenses_increment = st.slider('Expenses Increment (%)', 0, 100, 8) / 100
-return_percentage = st.slider('Return Percentage (%)', 0, 100, 3) / 100
+start_year = st.number_input('Started Working Year', min_value=2000, max_value=2024, value=2018, step=1)
+monthly_income = st.number_input('Monthly Income', min_value=1000, max_value=2000000, value=21000, step=1000)
+monthly_expenses = st.number_input('Monthly Expenses', min_value=1000, max_value=2000000, value=18000, step=500)
+salary_increment = st.slider('Salary Increment (%)', 0, 100, 10, 0.1) / 100
+expenses_increment = st.slider('Expenses Increment (%)', 0, 100, 8, 0.1) / 100
+return_percentage = st.slider('Return Percentage (%)', 0, 100, 3, 0.1) / 100
 
 if st.button('Calculate'):
     runway, savings_after_investment, extra_returns, savings_without_investment = calculate_financials(
@@ -35,7 +35,7 @@ if st.button('Calculate'):
     )
 
     st.write(f'Total Runway: {runway} years')
-    st.write(f'Total Saving after investment: ₹ {savings_after_investment:,.0f}')
-    st.write(f'Extra returns, if investment is made: ₹ {extra_returns:,.0f}')
-    st.write(f'Saving without investment: ₹ {savings_without_investment:,.0f}')
+    st.write(f'Total Saving after investment: ₹ {savings_after_investment:}')
+    st.write(f'Extra returns, if investment is made: ₹ {extra_returns:}')
+    st.write(f'Saving without investment: ₹ {savings_without_investment:}')
 
